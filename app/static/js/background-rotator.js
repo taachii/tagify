4ffba00c3267cb4cf1a61b40298eb3e898da6aff
taachii <taachii21@outlook.com>
2025-06-11 14:55:57 +0200

    Add background-rotator

--- app/static/js/background-rotator.js
+++ app/static/js/background-rotator.js
@@ -8,17 +8,29 @@ document.addEventListener("DOMContentLoaded", function () {
   ];
 
   let index = 0;
-  const changeInterval = 10000; // 15 sekund
+  const interval = 15000;
+  const fadeTime = 2000;
 
-  function changeBackground() {
-    const body = document.querySelector("body.hero-bg");
-    if (body) {
-      body.style.backgroundImage = `url('${images[index]}')`;
-      index = (index + 1) % images.length;
-    }
+  const bg1 = document.getElementById("bg1");
+  const bg2 = document.getElementById("bg2");
+  let showingBg1 = true;
+
+  function crossfade() {
+    const current = showingBg1 ? bg1 : bg2;
+    const next = showingBg1 ? bg2 : bg1;
+
+    next.style.backgroundImage = `url('${images[index]}')`;
+    next.style.opacity = 1;
+    current.style.opacity = 0;
+
+    showingBg1 = !showingBg1;
+    index = (index + 1) % images.length;
   }
 
-  // Start rotacji
-  changeBackground();
-  setInterval(changeBackground, changeInterval);
+  // Inicjalizacja
+  bg1.style.backgroundImage = `url('${images[0]}')`;
+  bg1.style.opacity = 1;
+  index = 1;
+
+  setInterval(crossfade, interval);
 });
